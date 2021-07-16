37bc8ecff7f44f8a367469b2178d394f1d77c4da
Oscar Mendoza <oscar.mendozaruiz@hotmail.com>
2021-07-16 06:55:00 -0500

    versionamiento de CORS

--- api/index.js
+++ api/index.js
@@ -3,13 +3,21 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const router = require('./server/routes/routes');
+const cors = require('cors')
+var corsOptions = {
+   origin: ['https://golky-front.herokuapp.com//', 'http://localhost:4200/', '*', 'https://golky-front.herokuapp.com/'
+   ]
+}
+app.options('*', cors(corsOptions))
+app.use(cors(corsOptions))
+
 // !important! 
 // you need to install the following libraries |express|[dotenv > if required]
 // or run this command >> npm i express dotenv 
 app.use('/api/v1',router);
 app.get('/' , (req , res)=>{
 
-   res.send('hello from simple server :)')
+   res.send('Hello from Golky Back :)')
 
 })
 
